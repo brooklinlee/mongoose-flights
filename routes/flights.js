832e5b8d74bdeb5b2695bd175ca54b04e210ccb1
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-16 17:32:26 -0600

    add delete functionality for flights

--- routes/flights.js
+++ routes/flights.js
@@ -5,6 +5,7 @@ const router = Router()
 router.get('/', flightsCtrl.index)
 router.get('/new', flightsCtrl.new)
 router.post('/', flightsCtrl.create)
+router.delete('/:flightId', flightsCtrl.delete)
 
 
 
