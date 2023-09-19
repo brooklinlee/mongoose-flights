f9f65cc86ef734dc3103dfe1756bfc89cad14868
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-19 16:33:25 -0600

    fix minor bugs with adding meals to the list of meals

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,5 @@
 import { Flight } from '../models/flight.js'
-// import { Meal } from '../models/meal.js'
+import { Meal } from '../models/meal.js'
 
 function index(req, res) {
   Flight.find({})
@@ -27,7 +27,6 @@ function create(req, res) {
   }
   Flight.create(req.body)
   .then(flight => {
-    console.log('CREATED FLIGHT:', flight)
     res.redirect('/flights')
   })
   .catch(error => {
@@ -49,16 +48,25 @@ function deleteFlight(req, res) {
 
 function show(req, res) {
   Flight.findById(req.params.flightId)
+  .populate('meals')
   .then(flight => {
+    Meal.find({_id: {$nin: flight.meals}})
+    .then(meals => {
     res.render('flights/show', {
       flight: flight,
-      title: 'Show Flight'
+      title: 'Show Flight',
+      meals: meals,
     })
   })
-  .catch(error => {
-    console.log(error)
+  .catch(err => {
+    console.log(err)
     res.redirect('/flights')
   })
+})
+.catch(err => {
+  console.log(err)
+  res.redirect('/flights')
+})
 }
 
 function edit(req, res) {
@@ -106,6 +114,25 @@ function createTicket(req, res) {
   })
 }
 
+function addMeal(req, res) {
+  Flight.findById(req.params.flightId)
+  .then(flight => {
+    flight.meals.push(req.body.mealId)
+    flight.save()
+    .then(() => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/flights')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
 export {
   index,
   newFlight as new,
@@ -115,4 +142,5 @@ export {
   edit,
   update,
   createTicket,
+  addMeal,
 }
